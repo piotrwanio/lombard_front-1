a69a42a0841886fcd42d0f2eb2470bd08dfc7ddc
Piotr <piotrwanio@gmail.com>
2019-04-15 14:22:31 +0200

    input form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PurchaseFormComponent } from './purchase-form/purchase-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PurchaseFormComponent
   ],
   imports: [
     BrowserModule,
