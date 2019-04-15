847bba43fee900ebd20f380b7f71863150f019ce
Patryk Moga <46868740+PatrykMoga@users.noreply.github.com>
2019-04-15 14:38:55 +0200

    selling-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SellingFormComponent } from './selling-form/selling-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SellingFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/selling-form/selling-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selling-form',
+  templateUrl: './selling-form.component.html',
+  styleUrls: ['./selling-form.component.scss']
+})
+export class SellingFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
